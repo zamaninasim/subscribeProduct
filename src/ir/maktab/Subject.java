fbdacac46b210cfef365e4fb442a6d6005c83148
nasim <zamaninasim213@gamail.com>
2021-11-24 19:58:40 +0330

    add subject class

---
+++ src/ir/maktab/Subject.java
@@ -0,0 +1,7 @@
+package ir.maktab;
+
+public interface Subject {
+    public void registerObserver(Observer observer);
+    public void removeObserver(Observer observer);
+    public void notifyObservers(String availability);
+}
