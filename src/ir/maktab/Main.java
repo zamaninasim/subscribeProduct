307a07609070a1f7d48730e6bbf095cff101ace1
nasim <zamaninasim213@gamail.com>
2021-11-24 20:00:17 +0330

    modify main

--- src/ir/maktab/Main.java
+++ src/ir/maktab/Main.java
@@ -1,4 +1,21 @@
 package ir.maktab;
 
+import ir.maktab.model.Person;
+import ir.maktab.model.Product;
+
 public class Main {
+    public static void main(String[] args) {
+        Person nasim = new Person("nasim");
+        Person ali = new Person("ali");
+
+        Product samsungMobile = new Product("Samsung", "Mobile", 0);
+
+        samsungMobile.registerObserver(nasim);
+        samsungMobile.registerObserver(ali);
+
+        samsungMobile.setCount(5);
+        System.out.println("********************************");
+        samsungMobile.setDiscount(true);
+
+    }
 }
